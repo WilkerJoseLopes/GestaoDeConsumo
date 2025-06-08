61088828231af0fd1ac681d6c38fbb92930f65ca
A046008 <153633086+A046008@users.noreply.github.com>
2025-06-08 18:24:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -45,9 +45,18 @@ HTML_TEMPLATE = """
             {casa_dados}
         ];
 
+        // Adiciona marcadores
+        casas.forEach(casa => {
+            const cor = casa.certificado === 'A+' ? 'green' :
+                        casa.certificado === 'A'  ? 'blue' :
+                        casa.certificado === 'B+' ? 'orange' :
+                        casa.certificado === 'B'  ? 'orange' :
+                        casa.certificado === 'B-' ? 'darkorange' :
+                        casa.certificado === 'C+' ? 'red' :
+                        'gray';
+
             const icone = L.icon({
-                iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-                shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+                iconUrl: `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-${cor}.png`,
                 iconSize: [25, 41],
                 iconAnchor: [12, 41],
                 popupAnchor: [0, -30]
